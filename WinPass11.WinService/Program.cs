783663657c9ac96bc63b72b5f38454f6de1e2def
Koji <cldoc507@gmail.com>
2021-06-27 21:15:52 -0400

    Add project files.

---
+++ WinPass11.WinService/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Timers;
+using Topshelf;
+
+namespace WinPass11.WinService
+{
+    public class TownCrier
+    {
+        readonly Timer _timer;
+        public TownCrier()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += (sender, eventArgs) => Console.WriteLine("It is {0} and all is well", DateTime.Now);
+        }
+        public void Start() { _timer.Start(); }
+        public void Stop() { _timer.Stop(); }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var rc = HostFactory.Run(x =>                                   //1
+            {
+                x.Service<TownCrier>(s =>                                   //2
+                {
+                    s.ConstructUsing(name => new TownCrier());                //3
+                    s.WhenStarted(tc => tc.Start());                         //4
+                    s.WhenStopped(tc => tc.Stop());                          //5
+                });
+                x.RunAsLocalSystem();                                       //6
+
+                x.SetDescription("Sample Topshelf Host");                   //7
+                x.SetDisplayName("WinPass11");                                  //8
+                x.SetServiceName("WinPass11");                                  //9
+            });                                                             //10
+
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());  //11
+            Environment.ExitCode = exitCode;
+        }
+    }
+}
